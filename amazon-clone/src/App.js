4e227070d7101d04e132125676ec0842a30cfeb9
Alshafei2430 <alshafei.2430@gmail.com>
2020-07-23 18:14:05 +0200

    first commit

---
+++ amazon-clone/src/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
+import Header from "./Header";
+
+function App() {
+  return (
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path="/checkout">
+            <h1>Checkout</h1>
+          </Route>
+          <Route path="/login">
+            <h1>Login page</h1>
+          </Route>
+          <Route path="/">
+            <Header />
+            <h1>HOME page!!!!!</h1>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
